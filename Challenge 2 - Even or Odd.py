430caac0af7130878b43c895f11cd6c6cf454d9f
Pratyush Raghuwanshi <pratyushraghuwanshi@gmail.com>
2023-05-24 11:24:12 -0700

    Delete Challenge 2 - Even or Odd.py

--- Challenge 2 - Even or Odd.py
+++
@@ -1,9 +0,0 @@
-# The exercise comes first (with a few extras if you want the extra challenge or want to spend more time), followed by a discussion. Enjoy!
-
-# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user
-
-num = int(input("what the number?"))
-if num%2 == 0:
-  print(f"Your number {num} is even")
-else:
-  print(f"Your number {num} is odd")
